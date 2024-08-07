3ad41441a2a44ea61af2e99c97fcda491bdb5431
RK <108023178+RAHUL-KAR-Kalyani@users.noreply.github.com>
2024-08-07 14:55:12 +0530

    updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,54 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+// src/App.js
+import React from 'react';
+import { useAppContext } from './AppContext';
 
+const App = () => {
+  const {
+    isAuthenticated,
+    currentUser,
+    items,
+    inputValue,
+    setInputValue,
+    login,
+    signout,
+    addItem,
+    removeItem,
+    clearItems
+  } = useAppContext();
+
+  return (
+    <div>
+      <button id="login-btn" onClick={login}>
+        Login
+      </button>
+      <button id="signout" onClick={signout}>
+        Signout
+      </button>
+      <div id="current-user">
+        Current user: {currentUser || ''}, isAuthenticated: {isAuthenticated ? 'Yes' : 'No'}
+      </div>
+      <input
+        id="shopping-input"
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <button onClick={addItem}>Add</button>
+      <button id="clear-list" onClick={clearItems}>
+        Clear
+      </button>
+      <ul>
+        {items.map((item) => (
+          <li key={item}>
+            {item}
+            <button id={`remove-${item}`} onClick={() => removeItem(item)}>
+              Remove
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
+// src/index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './components/App';
+import { AppProvider } from './components/AppContext';
 
-
-
+ReactDOM.render(
+  <AppProvider>
+    <App />
+  </AppProvider>,
+  document.getElementById('root')
+);
